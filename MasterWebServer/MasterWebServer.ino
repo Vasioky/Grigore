76b4bf3e331566300a0f007469e31ea946de2810
Vasile Postolache <vasioky@mail.ru>
2022-08-28 14:14:14 +0300

    switch func

--- MasterWebServer/MasterWebServer.ino
+++ MasterWebServer/MasterWebServer.ino
@@ -3,8 +3,7 @@
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEB };
-char server[] = "192, 168, 0, 178";    // name address for Google (using DNS)
-
+char server[] = "192.168.0.177";
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192, 168, 0, 178);
 IPAddress myDns(192, 168, 0, 1);
@@ -14,102 +13,98 @@ IPAddress myDns(192, 168, 0, 1);
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
-// Variables to measure the speed
-unsigned long beginMicros, endMicros;
-unsigned long byteCount = 0;
-bool printWebData = true;  // set to false for better speed measurement
-
+int pumpStatus = 0;
+int manualPin = 4;
 void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
-
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  // start the Ethernet connection:
-  Serial.println("Initialize Ethernet with DHCP:");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      while (true) {
-        delay(1); // do nothing, no point running without Ethernet hardware
-      }
+   Ethernet.begin(mac, ip, myDns);
+     // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
     }
-    if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-    // try to congifure using IP address instead of DHCP:
-    Ethernet.begin(mac, ip, myDns);
-  } else {
-    Serial.print("  DHCP assigned IP ");
-    Serial.println(Ethernet.localIP());
   }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(manualPin, INPUT);
+}
+
+void callSlave(String action){
   Serial.print("connecting to ");
   Serial.print(server);
   Serial.println("...");
-
-  // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
-    Serial.print("connected to ");
+    Serial.print(F("connected to "));
     Serial.println(client.remoteIP());
     // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
-    client.println("Connection: close");
+    String method = "/relay1" + action;
+    Serial.print("call ");
+    Serial.println(method); 
+    client.println("GET " + method + " HTTP/1.1");
     client.println();
   } else {
     // if you didn't get a connection to the server:
-    Serial.println("connection failed");
+    Serial.println(F("connection failed"));
   }
-  beginMicros = micros();
+  /*Serial.println(F("read resp:"));
+   while (client.connected() || client.available()) { //connected or data available
+    char c = client.read(); //gets byte from ethernet buffer
+    if(c == '\n'){
+      break;
+    }
+    Serial.print(c); //prints byte to serial monitor 
+  }*/
+  delay(1);
+  client.println("Host: 192.168.0.177");
+  client.println(F("Connection: close"));
+  client.println();
+  delay(1);
+  client.stop();
+  Serial.println(F("Connection closed"));
+  pumpStatus = action == "on" ? 1 : 0;
 }
 
 void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  int len = client.available();
-  if (len > 0) {
-    byte buffer[80];
-    if (len > 80) len = 80;
-    client.read(buffer, len);
-    if (printWebData) {
-      Serial.write(buffer, len); // show in the serial monitor (slows some boards)
+  int manualSwitch = digitalRead(manualPin);
+  int lowLevel = digitalRead(2);
+  int highLevel = digitalRead(3);
+  
+  Serial.print("Manual:");
+  Serial.print(manualSwitch);
+  Serial.print(" lowLevel:");
+  Serial.print(lowLevel);
+  Serial.print(" highLevel:");
+  Serial.print(highLevel);
+  Serial.print(" pump:");
+  Serial.println(pumpStatus);
+  if(lowLevel == HIGH && highLevel == LOW){
+    //this is should not happen any time, but in case it happens we need to go in error status otherwise we get in loop wich starts and stopes the slave
+    Serial.println("ERROR");
+  }else{
+    if(manualSwitch == HIGH && pumpStatus == 0){
+      Serial.println("turn onn");
+      callSlave("on");
+    }else{
+      
+      if(lowLevel == HIGH && pumpStatus == 0){
+        Serial.println("turn onn");
+        callSlave("on");
+      }else if(highLevel == LOW && pumpStatus == 1){
+        Serial.println("turn off");
+        callSlave("off");
+      }
     }
-    byteCount = byteCount + len;
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    endMicros = micros();
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    Serial.print("Received ");
-    Serial.print(byteCount);
-    Serial.print(" bytes in ");
-    float seconds = (float)(endMicros - beginMicros) / 1000000.0;
-    Serial.print(seconds, 4);
-    float rate = (float)byteCount / seconds / 1000.0;
-    Serial.print(", rate = ");
-    Serial.print(rate);
-    Serial.print(" kbytes/second");
-    Serial.println();
-
-    // do nothing forevermore:
-    while (true) {
-      delay(1);
-    }
+    // if you get a connection, report back via serial:
+    delay(500);
   }
 }
